4f215fe79a802db367175efe65103fa883235ee1
Juri Lelli <juri.lelli@redhat.com>
2025-06-12 17:12:22 +0200

    Add a plugin for old fashion cscope
    For when making the LSP work it's just too painful.

--- init.lua
+++ init.lua
@@ -618,6 +618,23 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'dhananjaylatkar/cscope_maps.nvim',
+    dependencies = {
+      'nvim-telescope/telescope.nvim', -- optional [for picker="telescope"]
+      'ibhagwan/fzf-lua', -- optional [for picker="fzf-lua"]
+      'echasnovski/mini.pick', -- optional [for picker="mini-pick"]
+      'folke/snacks.nvim', -- optional [for picker="snacks"]
+    },
+    opts = {
+      skip_input_prompt = true,
+      cscope = {
+        picker = 'telescope',
+        skip_picker_for_single_result = true,
+      },
+    },
+  },
+
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
