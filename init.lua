f777f85503446c9d8848e200dfeb1db46a752f52
Juri Lelli <juri.lelli@redhat.com>
2025-06-16 15:47:20 +0200

    Reopen a file where you left off
    It usually saves time.

--- init.lua
+++ init.lua
@@ -355,6 +355,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Reopen a file where you left off
+vim.api.nvim_create_autocmd('BufReadPost', {
+  pattern = { '*' },
+  callback = function()
+    if vim.fn.line '\'"' > 1 and vim.fn.line '\'"' <= vim.fn.line '$' then
+      vim.api.nvim_exec('normal! g`"', false)
+    end
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
